c34559fa5b1ec9abc7d0ae90d50c95e316adc879
芸 <118110691+0921zyun@users.noreply.github.com>
2022-11-14 19:18:53 +0800

    Add files via upload

---
+++ ~AutoRecover.28.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	//int a=0;
+	//int b=2;
+	//if(a==1)
+	//{
+	//	if(b==2)
+	//	printf("hehe\n");
+	//}
+	//else
+	//	printf("haha\n");
+	/*int i;
+	printf("输入一个数字:\n");
+	scanf("%d",&i);
+	if(i%2!=0)
+		printf("奇数\n");
+	else
+		printf("偶数\n");
+*/
+	/*int i;
+	for(i=1;i<=100;i++)
+	{
+	if(i%2!=0)
+		printf("%d\t",i);
+	}*/
+	
+return 0;
+}
